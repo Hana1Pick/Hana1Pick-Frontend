dd3d1496d2753c1f147fed4fe87d3fe1f45c0f2f
juhye <rlawngp124@gmail.com>
2024-06-20 11:03:24 +0900

    chroe: 프로젝트 생성

---
+++ src/type/commonType.tsx
@@ -0,0 +1,22 @@
+// export type LogonAction = 
+//      {type:"RESET", value:StateType}
+//     | {type:"LOGON", value:StateType}
+//     | {type:"LOGOUT", value:StateType};
+
+//     export type PhotoType={
+//         "albumId": number;
+//         "id": number;
+//         "title": string;
+//         "url": string;
+//         "thumbnailUrl": string;
+//     }
+    
+//     export type AlbumType={
+//         userId:number;
+//         id:number;
+//         title:string;
+//     }
+
+export type CommonType={
+    userId:number;
+}
