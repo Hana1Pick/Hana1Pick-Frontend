b75811a6bcc9d08b330679ecb067c8b02913bc31
kgw0124 <kgw0124@kakao.com>
2024-06-27 11:29:56 +0900

    refactor: CommonBtn에 disabled 속성 추가

--- src/components/button/CommonBtn.tsx
+++ src/components/button/CommonBtn.tsx
@@ -2,14 +2,15 @@ type CommonBtnData = {
   type: string;
   value: string;
   onClick: () => void;
+  disabled: boolean;
 };
 
-function CommonBtn({ type, value, onClick }: CommonBtnData) {
+function CommonBtn({ type, value, onClick, disabled }: CommonBtnData) {
   const id = type === 'pink' ? 'basicBtn1' : 'basicBtn2';
 
   return (
     <>
-      <button id={id} onClick={onClick}>
+      <button id={id} onClick={onClick} disabled={disabled}>
         {value}
       </button>
     </>
