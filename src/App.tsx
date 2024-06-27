e93a8443cdd734abc4a6f15d40a3bbc064b265eb
kgw0124 <kgw0124@kakao.com>
2024-06-27 11:31:46 +0900

    feat: AccountContextProvider 하단에 계좌 이체 프로세스 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 // 계좌 이체
+import { AccountContextProvider } from './contexts/AccountContextProvider';
 import SelectAccountPage from './pages/account/SelectAccountPage';
 import SearchAccountPage from './pages/account/SearchAccoutPage';
+import GetAmountPage from './pages/account/GetAmountPage';
+import CashOutPage from './pages/account/CashOutPage';
+import CashOutPatternPage from './pages/account/CashOutPatternPage';
+import CashOutResultPage from './pages/account/CashOutResultPage';
 
 function App() {
   return (
@@ -13,10 +18,20 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<MainPage />} />
-          <Route path='/cash-out/account' element={<SelectAccountPage />} />
           <Route
-            path='/cash-out/account-query'
-            element={<SearchAccountPage />}
+            path='/cash-out/*'
+            element={
+              <AccountContextProvider>
+                <Routes>
+                  <Route path='account' element={<SelectAccountPage />} />
+                  <Route path='account-query' element={<SearchAccountPage />} />
+                  <Route path='amount' element={<GetAmountPage />} />
+                  <Route path='' element={<CashOutPage />} />
+                  <Route path='pattern' element={<CashOutPatternPage />} />
+                  <Route path='result' element={<CashOutResultPage />} />
+                </Routes>
+              </AccountContextProvider>
+            }
           />
         </Routes>
       </BrowserRouter>
