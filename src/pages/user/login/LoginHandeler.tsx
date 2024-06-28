5553dca245b75f17ecd99a368e09d9aa94efe16f
allzeroyou <ekdudd119@naver.com>
2024-06-28 18:03:00 +0900

    feat: 카카오 OAuth 로그인 핸들러 및 사용자 정보 저장 기능 추가
    - 카카오 API와의 OAuth 토큰 교환 구현
    - 로그인 후 사용자 정보 가져오기 기능 추가
    - userIdx, name, email을 localStorage에 저장
    - 로그인 성공 후 메인페이지로 리디렉션

---
+++ src/pages/user/login/LoginHandeler.tsx
@@ -0,0 +1,61 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { stringify } from "querystring";
+import { get } from "http";
+
+const LoginHandler = (props:any) => {
+  const code = new URL(window.location.href).searchParams.get("code");
+  const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+
+  const url = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${code}`;
+
+  const navigate = useNavigate();
+
+
+  axios.post(url, null, {headers:{
+    "Content-Type": "application/x-www-form-urlencoded"
+  }})
+  .then((response) => {
+    console.log("1 end");
+    getUserInfo(response.data.access_token);
+  })
+  .catch((error) => {console.log(error)})
+
+  const getUserInfo = (accessToken:string) => {
+    console.log("2 start");
+
+    const url2 = `http://${process.env.REACT_APP_BESERVERURI}/api/user/login`;
+    const data = {
+      accessToken: accessToken
+    }
+
+    axios
+      .get(url2, { params: data })
+      .then((response) => {
+        console.log(response);
+        const { userIdx, name, email } = response.data.data;
+        localStorage.setItem("userIdx", userIdx);
+        localStorage.setItem("name", name);
+        localStorage.setItem("email", email);
+
+        navigate("/")
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="LoginHandler">
+      <div className="notice">
+        <p>로그인 중입니다.</p>
+        <p>잠시만 기다려주세요.</p>
+        <div className="spinner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginHandler;
