252560e90516f8535eb4c5e5814255e9a155c53f
allzeroyou <ekdudd119@naver.com>
2024-06-28 18:01:53 +0900

    feat: 로컬스토리지에 사용자 정보(idx, 이름, 이메일) 저장

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,21 +1,27 @@
-function MainPage(){
-    <script>
-    
-        
-    </script>
-    return(
-        <>
-            <div>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <h1>
-                    여기는 메인페이지
-                </h1>
-                <div>
-                    <button id="basicBtn1">Click!</button>
-                </div>
-            </div>
-        </>
-    );
-}
+const MainPage = () => {
+  console.log(
+    "localStorage에 저장된 userIdx:",
+    localStorage.getItem("userIdx")
+  );
+  console.log(
+    "localStorage에 저장된 name:",
+    localStorage.getItem("name")
+  );
+  console.log(
+    "localStorage에 저장된 email:",
+    localStorage.getItem("email")
+  );
+  return (
+    <>
+      <div>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <h1>여기는 메인페이지 // TODO: 계좌 목록을 가져와야함</h1>
+        <div>
+          <button id="basicBtn1">Click!</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default MainPage;
