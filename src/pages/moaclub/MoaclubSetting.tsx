201836af62663b5f4721b50765020ab0c93cd9ce
soyeonvv <sovvworld@gmail.com>
2024-07-01 01:14:41 +0900

    feat: 모아클럽 환경설정 페이지

--- src/pages/moaclub/MoaclubSetting.tsx
+++ src/pages/moaclub/MoaclubSetting.tsx
@@ -1,28 +1,40 @@
-import Header from "../../layouts/MoaclubHeader4";
+import Header from '../../layouts/MoaclubHeader4';
 import './MoaclubStyle.scss';
-import '../../common/styles/scss/CommonStyle.scss'
+import '../../common/styles/scss/CommonStyle.scss';
+import righticon from '../../assets/images/common/righticon.png';
 import { useNavigate } from 'react-router-dom';
-import { useState } from "react";
-import styled from '@emotion/styled';
+import { useState } from 'react';
 
 function MoaclubSetting() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const next = () => {
+	const next = () => {};
 
-  };
-
-  return(
-      <>
-        <Header value="모아클럽 관리" disabled={false} />
-        <div className="content">
-
-        </div>
-        
-        
-
-      </>
-  )
+	return (
+		<>
+			<Header value='모아클럽 관리' disabled={false} />
+			<div className='content'>
+				<div className='moaclubSettingContainer'>
+					<div>
+						모아클럽 투표
+						<img className='rightIcon' alt='right-icon' src={righticon} />
+					</div>
+					<div>
+						모아클럽 수정
+						<img className='rightIcon' alt='right-icon' src={righticon} />
+					</div>
+					<div>
+						자동이체 설정
+						<img className='rightIcon' alt='right-icon' src={righticon} />
+					</div>
+					<div>
+						모아클럽 사용 종료
+						<img className='rightIcon' alt='right-icon' src={righticon} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default MoaclubSetting;
