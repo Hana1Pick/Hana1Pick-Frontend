029682f844e56340d3a1c5e16c92ef3e50359e69
allzeroyou <ekdudd119@naver.com>
2024-07-02 07:08:01 +0900

    feat: 하단 메뉴바 hover 적용

--- src/components/menubar/MenuBar.tsx
+++ src/components/menubar/MenuBar.tsx
@@ -1,25 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
-import bellIcon from "../../assets/images/main/bell_icon.png";
-import homeIcon from "../../assets/images/main/home_icon.png";
-import userIcon from "../../assets/images/main/user_icon.png";
-import qrIcon from "../../assets/images/main/qr_icon.png";
-import { useNavigate } from 'react-router-dom';
+
+import beBellIcon from "../../assets/images/menubar/b_alarm.png";
+import beHomeIcon from "../../assets/images/menubar/b_home.png";
+import beQrIcon from "../../assets/images/menubar/b_qr.png";
+import beUserIcon from "../../assets/images/menubar/b_my.png";
+
+import afHomeIcon from "../../assets/images/menubar/a_home.png";
+import afQrIcon from "../../assets/images/menubar/a_qr.png";
+import afUserIcon from "../../assets/images/menubar/a_my.png";
+import afBellIcon from "../../assets/images/menubar/a_alarm.png";
 
 const MenuBar = () => {
+  const [icons, setIcons] = useState({
+    home: beHomeIcon,
+    qr: beQrIcon,
+    bell: beBellIcon,
+    user: beUserIcon,
+  });
+
+  const handleMouseEnter = (icon: string) => {
+    switch (icon) {
+      case 'home':
+        setIcons((prev) => ({ ...prev, home: afHomeIcon }));
+        break;
+      case 'qr':
+        setIcons((prev) => ({ ...prev, qr: afQrIcon }));
+        break;
+      case 'bell':
+        setIcons((prev) => ({ ...prev, bell: afBellIcon }));
+        break;
+      case 'user':
+        setIcons((prev) => ({ ...prev, user: afUserIcon }));
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleMouseLeave = (icon: string) => {
+    switch (icon) {
+      case 'home':
+        setIcons((prev) => ({ ...prev, home: beHomeIcon }));
+        break;
+      case 'qr':
+        setIcons((prev) => ({ ...prev, qr: beQrIcon }));
+        break;
+      case 'bell':
+        setIcons((prev) => ({ ...prev, bell: beBellIcon }));
+        break;
+      case 'user':
+        setIcons((prev) => ({ ...prev, user: beUserIcon }));
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="menu-bar">
-      <div className="menu-item">
-        <img src={homeIcon} alt="home-icon" style={{width: "3rem"}}/>
+      <div
+        className="menu-item"
+        onMouseEnter={() => handleMouseEnter('home')}
+        onMouseLeave={() => handleMouseLeave('home')}
+      >
+        <img src={icons.home} alt="home-icon" style={{ width: "2.5rem" }} />
       </div>
-      <div className="menu-item">
-        <img src={qrIcon} alt="qr-icon" style={{width: "3.4rem"}}/>
+      <div
+        className="menu-item"
+        onMouseEnter={() => handleMouseEnter('qr')}
+        onMouseLeave={() => handleMouseLeave('qr')}
+      >
+        <img src={icons.qr} alt="qr-icon" style={{ width: "3rem", marginTop: "0.2rem" }} />
       </div>
-      <div className="menu-item">
-        <img src={bellIcon} alt="bell-icon" style={{width: "2.5rem"}}/>
+      <div
+        className="menu-item"
+        onMouseEnter={() => handleMouseEnter('bell')}
+        onMouseLeave={() => handleMouseLeave('bell')}
+      >
+        <img src={icons.bell} alt="bell-icon" style={{ width: "2.2rem", marginTop: "0.3rem" }} />
       </div>
-      <div className="menu-item">
-        <img src={userIcon} alt="user-icon" style={{width: "2.5rem"}}/>
+      <div
+        className="menu-item"
+        onMouseEnter={() => handleMouseEnter('user')}
+        onMouseLeave={() => handleMouseLeave('user')}
+      >
+        <img src={icons.user} alt="user-icon" style={{ width: "2.2rem", marginTop: "0.4rem" }} />
       </div>
     </div>
   );
