6510d2813e61b14e9ddf49299c5a1b3094a800af
allzeroyou <ekdudd119@naver.com>
2024-07-02 07:07:41 +0900

    feat: 계좌 목록 스와이프 기능 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,14 +4,12 @@ import { useState } from "react";
 import { To, useNavigate } from "react-router-dom";
 import Logo from "../assets/images/common/CircleLogo.png";
 import menuIcon from "../assets/images/main/hambuger_icon.png";
-
 import celubIcon from "../assets/images/main/main_celub_icon.png";
-
 import moaIcon from "../assets/images/main/main_moa_icon.png";
 import MenuBar from "../components/menubar/MenuBar";
-
 import "./style.scss";
 import { useSwipeable } from "react-swipeable";
+import NavBar from "../components/hambuger/NavBar";
 
 interface Account {
   id: string;
@@ -22,11 +20,13 @@ interface Account {
 }
 
 const MainPage = () => {
-  const [accounts, setAccounts] = useState<Account[]>([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [accounts, setAccounts] = useState<Account[]>([]); // 계좌 데이터
+  const [currentIndex, setCurrentIndex] = useState(0); // 스와이프: 현재 계좌 인덱스
   const navigate = useNavigate();
+  // 사용자 정보
   const name = localStorage.getItem("name");
   const profile = localStorage.getItem("profile");
+  const [isNavOpen, setIsNavOpen] = useState(false); // 햄버거 메뉴바
 
   useEffect(() => {
     const userIdx = localStorage.getItem("userIdx");
@@ -52,16 +52,17 @@ const MainPage = () => {
         console.error(error);
       });
   }, []);
+  // 페이지 이동 함수
   const handleNavigate = (path: To) => {
     navigate(path);
   };
+  // 스와이프 이벤트 핸들러
   const handlers = useSwipeable({
     onSwipedLeft: () =>
       setCurrentIndex((prev) => Math.min(prev + 1, accounts.length - 1)),
     onSwipedRight: () => setCurrentIndex((prev) => Math.max(prev - 1, 0)),
     trackMouse: true,
   });
-  
 
   console.log(
     "localStorage에 저장된 userIdx:",
@@ -82,28 +83,28 @@ const MainPage = () => {
             {profile ? (
               <img className="profile-pic" src={profile} alt="profile-pic" />
             ) : (
-              <div className="default-pic"></div>
+              <img className="default-pic" src={Logo} alt="default-pic" />
             )}
           </div>
           <h3 className="user-name">{name} 님</h3>
-          <div className="menu-icon">
+          <div className="menu-icon" onClick={() => setIsNavOpen(true)}>
+            {" "}
+            {/* 햄버거 메뉴바 부분 */}
             <img src={menuIcon} alt="menu-icon" />
           </div>
         </div>
 
         <div className="account-container" {...handlers}>
-   
+          <div
+            className="account-details"
+            style={{
+              transform: `translateX(-${currentIndex * 100}%)`,
+            }}
+          >
             {accounts.length > 0 ? (
               accounts.map((account) => (
-                <div
-                className="account-details"
-                style={{
-                  transform: `translateX(-${currentIndex * 100}%)`,
-                  transition: "transform 0.3s ease-in-out",
-                }}
-              >
-                <div className="accountBox" key={account.accountId} >
-                  <div>
+                <div className="accountBox" key={account.accountId}>
+                  <div className="imgContainer">
                     <img src={Logo} alt="logo" style={{ width: "2.25rem" }} />
                   </div>
                   <div className="accountDetail">
@@ -115,21 +116,14 @@ const MainPage = () => {
                     <h3 className="account-balance">
                       {account.balance.toLocaleString()}원
                     </h3>
-                    <button id="basicBtn1" className="send-button">
+                    {/* // TODO: 계좌별 송금 페이지로 이동 */}
+                    <button className="send-button" onClick={() => {}}>
                       보내기
                     </button>
                   </div>
                 </div>
-                </div>
               ))
             ) : (
-              <div
-              className="account-details"
-              style={{
-                transform: `translateX(-${currentIndex * 100}%)`,
-                transition: "transform 0.3s ease-in-out",
-              }}
-            >
               <div className="accountBox">
                 <div>
                   <img src={Logo} alt="logo" style={{ width: "2.25rem" }} />
@@ -143,9 +137,18 @@ const MainPage = () => {
                   </button>
                 </div>
               </div>
-              </div>
             )}
           </div>
+          <div className="scrollbar">
+            <div
+              className="scrollbar-indicator"
+              style={{
+                width: `${100 / accounts.length}%`,
+                transform: `translateX(${currentIndex * 100}%)`,
+              }}
+            ></div>
+          </div>
+        </div>
 
         <div className="promotions">
           <div className="promotion">
@@ -155,7 +158,10 @@ const MainPage = () => {
               <p className="accountTitle" style={{ fontSize: "0.9rem" }}>
                 최애와 함께 저축 습관 들이기!
               </p>
-              <button onClick={() => handleNavigate("/celub/")}>
+              <button
+                onClick={() => handleNavigate("/celub/")}
+                style={{ fontWeight: "500" }}
+              >
                 셀럽로그 시작하기
               </button>
             </div>
@@ -166,7 +172,10 @@ const MainPage = () => {
               <p className="accountTitle" style={{ fontSize: "0.9rem" }}>
                 최애가 같다면 함께 쓰는 모임통장!
               </p>
-              <button onClick={() => handleNavigate("/moaclub/opening")}>
+              <button
+                onClick={() => handleNavigate("/moaclub/opening")}
+                style={{ fontWeight: "500" }}
+              >
                 모아클럽 시작하기
               </button>
             </div>
@@ -174,6 +183,8 @@ const MainPage = () => {
         </div>
       </div>
       <MenuBar />
+      <NavBar isOpen={isNavOpen} onClose={() => setIsNavOpen(false)} />{" "}
+      {/* 추가된 부분 */}
     </>
   );
 };
