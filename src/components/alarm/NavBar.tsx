ef7054b59bede8016ae9c6eeced7eff63c834ce2
allzeroyou <ekdudd119@naver.com>
2024-07-03 12:38:09 +0900

    feat: 햄버거 메뉴바 → 알림 창으로 UI/UX 변경

--- src/components/alarm/NavBar.tsx
+++ src/components/alarm/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import closeIcon from '../../assets/images/main/close_Frame.png';
+import closeIcon from '../../assets/images/alarm/close_icon.png';
 import './style.scss';
 
 interface NavBarProps {
@@ -12,21 +12,21 @@ const NavBar: React.FC<NavBarProps> = ({ isOpen, onClose }) => {
   return (
     <div className={`nav-bar ${isOpen ? 'open' : ''}`}>
       <div className="nav-header">
-        <img src={closeIcon} alt="close" className="close-btn" onClick={onClose} />
+        <img src={closeIcon} alt="close" className="close-btn" onClick={onClose} style={{width: "34px"}}/>
       </div>
-      {/* // TODO: 페이지에 맞는 라우팅 주소로 변경 필요! */}
+      {/* // TODO: 알람 페이지로, 알람 내역 가져와서 데이터 변경 필요! */}
       <ul className="nav-links">
         <li>
-          <Link to="/" onClick={onClose}>총 자산</Link>
+          <Link to="/" >어쩌구 알림1</Link>
         </li>
         <li>
-          <Link to="/ranking" onClick={onClose}>최애 실시간 랭킹</Link>
+          <Link to="/ranking" >최애 실시간 랭킹</Link>
         </li>
         <li>
-          <Link to="/qr" onClick={onClose}>QR 송금 만들기</Link>
+          <Link to="/qr" >QR 송금 만들기</Link>
         </li>
         <li>
-          <Link to="/photo" onClick={onClose}>최애와 한 컷</Link>
+          <Link to="/photo">최애와 한 컷</Link>
         </li>
       </ul>
     </div>
