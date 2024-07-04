66d6f1e39af1e819a0ef552ea90446bcc6ba029e
soyeonvv <sovvworld@gmail.com>
2024-07-04 15:50:09 +0900

    chore: 알림 설정 취소

--- src/components/menubar/MenuBar.tsx
+++ src/components/menubar/MenuBar.tsx
@@ -1,94 +1,106 @@
 import React, { useState } from 'react';
 import './style.scss';
 
-import beBellIcon from "../../assets/images/menubar/b_alarm.png";
-import beHomeIcon from "../../assets/images/menubar/b_home.png";
-import beQrIcon from "../../assets/images/menubar/b_qr.png";
-import beUserIcon from "../../assets/images/menubar/b_my.png";
+import beBellIcon from '../../assets/images/menubar/b_alarm.png';
+import beHomeIcon from '../../assets/images/menubar/b_home.png';
+import beQrIcon from '../../assets/images/menubar/b_qr.png';
+import beUserIcon from '../../assets/images/menubar/b_my.png';
 
-import afHomeIcon from "../../assets/images/menubar/a_home.png";
-import afQrIcon from "../../assets/images/menubar/a_qr.png";
-import afUserIcon from "../../assets/images/menubar/a_my.png";
-import afBellIcon from "../../assets/images/menubar/a_alarm.png";
+import afHomeIcon from '../../assets/images/menubar/a_home.png';
+import afQrIcon from '../../assets/images/menubar/a_qr.png';
+import afUserIcon from '../../assets/images/menubar/a_my.png';
+import afBellIcon from '../../assets/images/menubar/a_alarm.png';
 
 const MenuBar = () => {
-  const [icons, setIcons] = useState({
-    home: beHomeIcon,
-    qr: beQrIcon,
-    bell: beBellIcon,
-    user: beUserIcon,
-  });
+	const [icons, setIcons] = useState({
+		home: beHomeIcon,
+		qr: beQrIcon,
+		bell: beBellIcon,
+		user: beUserIcon,
+	});
 
-  const handleMouseEnter = (icon: string) => {
-    switch (icon) {
-      case 'home':
-        setIcons((prev) => ({ ...prev, home: afHomeIcon }));
-        break;
-      case 'qr':
-        setIcons((prev) => ({ ...prev, qr: afQrIcon }));
-        break;
-      case 'bell':
-        setIcons((prev) => ({ ...prev, bell: afBellIcon }));
-        break;
-      case 'user':
-        setIcons((prev) => ({ ...prev, user: afUserIcon }));
-        break;
-      default:
-        break;
-    }
-  };
+	const handleMouseEnter = (icon: string) => {
+		switch (icon) {
+			case 'home':
+				setIcons((prev) => ({ ...prev, home: afHomeIcon }));
+				break;
+			case 'qr':
+				setIcons((prev) => ({ ...prev, qr: afQrIcon }));
+				break;
+			case 'bell':
+				setIcons((prev) => ({ ...prev, bell: afBellIcon }));
+				break;
+			case 'user':
+				setIcons((prev) => ({ ...prev, user: afUserIcon }));
+				break;
+			default:
+				break;
+		}
+	};
 
-  const handleMouseLeave = (icon: string) => {
-    switch (icon) {
-      case 'home':
-        setIcons((prev) => ({ ...prev, home: beHomeIcon }));
-        break;
-      case 'qr':
-        setIcons((prev) => ({ ...prev, qr: beQrIcon }));
-        break;
-      case 'bell':
-        setIcons((prev) => ({ ...prev, bell: beBellIcon }));
-        break;
-      case 'user':
-        setIcons((prev) => ({ ...prev, user: beUserIcon }));
-        break;
-      default:
-        break;
-    }
-  };
+	const handleMouseLeave = (icon: string) => {
+		switch (icon) {
+			case 'home':
+				setIcons((prev) => ({ ...prev, home: beHomeIcon }));
+				break;
+			case 'qr':
+				setIcons((prev) => ({ ...prev, qr: beQrIcon }));
+				break;
+			case 'bell':
+				setIcons((prev) => ({ ...prev, bell: beBellIcon }));
+				break;
+			case 'user':
+				setIcons((prev) => ({ ...prev, user: beUserIcon }));
+				break;
+			default:
+				break;
+		}
+	};
 
-  return (
-    <div className="menu-bar">
-      <div
-        className="menu-item"
-        onMouseEnter={() => handleMouseEnter('home')}
-        onMouseLeave={() => handleMouseLeave('home')}
-      >
-        <img src={icons.home} alt="home-icon" style={{ width: "2.5rem" }} />
-      </div>
-      <div
-        className="menu-item"
-        onMouseEnter={() => handleMouseEnter('qr')}
-        onMouseLeave={() => handleMouseLeave('qr')}
-      >
-        <img src={icons.qr} alt="qr-icon" style={{ width: "3rem", marginTop: "0.2rem" }} />
-      </div>
-      <div
-        className="menu-item"
-        onMouseEnter={() => handleMouseEnter('bell')}
-        onMouseLeave={() => handleMouseLeave('bell')}
-      >
-        <img src={icons.bell} alt="bell-icon" style={{ width: "2.2rem", marginTop: "0.3rem" }} />
-      </div>
-      <div
-        className="menu-item"
-        onMouseEnter={() => handleMouseEnter('user')}
-        onMouseLeave={() => handleMouseLeave('user')}
-      >
-        <img src={icons.user} alt="user-icon" style={{ width: "2.2rem", marginTop: "0.4rem" }} />
-      </div>
-    </div>
-  );
+	return (
+		<div className='menu-bar'>
+			<div
+				className='menu-item'
+				onMouseEnter={() => handleMouseEnter('home')}
+				onMouseLeave={() => handleMouseLeave('home')}
+			>
+				<img src={icons.home} alt='home-icon' style={{ width: '2.5rem' }} />
+			</div>
+			<div
+				className='menu-item'
+				onMouseEnter={() => handleMouseEnter('qr')}
+				onMouseLeave={() => handleMouseLeave('qr')}
+			>
+				<img
+					src={icons.qr}
+					alt='qr-icon'
+					style={{ width: '3rem', marginTop: '0.2rem' }}
+				/>
+			</div>
+			<div
+				className='menu-item'
+				onMouseEnter={() => handleMouseEnter('bell')}
+				onMouseLeave={() => handleMouseLeave('bell')}
+			>
+				<img
+					src={icons.bell}
+					alt='bell-icon'
+					style={{ width: '2.2rem', marginTop: '0.3rem' }}
+				/>
+			</div>
+			<div
+				className='menu-item'
+				onMouseEnter={() => handleMouseEnter('user')}
+				onMouseLeave={() => handleMouseLeave('user')}
+			>
+				<img
+					src={icons.user}
+					alt='user-icon'
+					style={{ width: '2.2rem', marginTop: '0.4rem' }}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default MenuBar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import "../src/common/styles/scss/CommonStyle.scss";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "react-datepicker/dist/react-datepicker.css";
+import '../src/common/styles/scss/CommonStyle.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const kakaoKey = process.env.REACT_APP_KAKAO_KEY_SHARE;
 console.log(kakaoKey);
@@ -11,12 +11,12 @@ window.Kakao.init(kakaoKey);
 window.Kakao.isInitialized();
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 reportWebVitals();
