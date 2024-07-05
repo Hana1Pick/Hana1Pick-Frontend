a7ee6260cfd604603bfe37804c8513c35f547c4b
allzeroyou <ekdudd119@naver.com>
2024-07-06 00:00:16 +0900

    feat: StageSpinner 로딩 화면 구현

--- src/components/pageLoding/pageLoading.tsx
+++ src/components/pageLoding/pageLoading.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { StageSpinner } from "react-spinners-kit";
 import "./style.scss"; // 스피너 스타일이 포함된 CSS 파일
 
 const PageLoadingSpinner = () => {
   return (
-  <div className="pageSpinner"></div>
-  )
+    <div className="spinner-container">
+      <StageSpinner size={60} color="#019178" loading={true} />
+    </div>
+  );
 };
 
 export default PageLoadingSpinner;
