7f398980f2115120009c88e4d61cead1f08fab72
juhye <rlawngp124@gmail.com>
2024-07-07 23:55:53 +0900

    feat: 헤더에 뒤로가기 적용

--- src/layouts/CelubHeader.tsx
+++ src/layouts/CelubHeader.tsx
@@ -1,12 +1,18 @@
 import arrow from'../assets/images/celub/arrow.png';
+import { useNavigate } from 'react-router-dom';
+
 interface CelubHeaderProps {
     value: string;
 }
 function CelubHeader({ value }: CelubHeaderProps){
+    const navigate = useNavigate(); // useNavigate 훅 사용
+    const handleBackClick = () => {
+      navigate(-1); // 뒤로 가기
+    };
     return(
         <>
             <div id="celubHeader1">
-                <img className="back" src={arrow} />
+                <img className="back" src={arrow} onClick={handleBackClick}/>
                 <h4>{value}</h4>
             </div>
         </>
--- src/layouts/CelubHeader2.tsx
+++ src/layouts/CelubHeader2.tsx
@@ -1,10 +1,16 @@
 import arrow from'../assets/images/celub/arrow.png';
 import setting from '../assets/images/celub/setting.png';
+import { useNavigate } from 'react-router-dom'; 
+
 function CelubHeader2(){
+    const navigate = useNavigate(); // useNavigate 훅 사용
+    const handleBackClick = () => {
+      navigate(-1); // 뒤로 가기
+    };
     return(
         <>
             <div id="celubHeader1">
-                <img id="arrow_loc" src={arrow} />
+                <img id="arrow_loc" src={arrow} onClick={handleBackClick}/>
                 <img id="setting" src={setting}/>
             </div>
         </>
