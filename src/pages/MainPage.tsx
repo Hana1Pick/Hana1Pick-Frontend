9b9e0d70950d07e9fc9732e22ba0ea459f53b970
allzeroyou <ekdudd119@naver.com>
2024-07-07 11:44:24 +0900

    feat: 메인페이지 상품 개설 애니메이션 적용, 상품 icon 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { To, useNavigate } from 'react-router-dom';
 import hanaLogo from '../assets/images/common/hanaBankLogo.png';
 import menuIcon from '../assets/images/alarm/black_alarm_icon.png';
 import celubIcon from '../assets/images/main/main_celub_icon.png';
-import moaIcon from '../assets/images/main/main_moa_icon.png';
+import moaIcon from '../assets/images/main/main_moa_logo.png';
 import MenuBar from '../components/menubar/MenuBar';
 import './style.scss';
 import { useSwipeable } from 'react-swipeable';
@@ -13,7 +12,6 @@ import NavBar from '../components/alarm/NavBar';
 import Exchange from '../components/exchange';
 import LoadingSpinner from '../components/loading/boxLoading'; // 로딩 스피너 컴포넌트 임포트
 import { NotificationType } from '../type/commonType';
-
 
 interface Account {
 	id: string;
@@ -28,160 +26,118 @@ const MainPage = () => {
   const [currentIndex, setCurrentIndex] = useState(0); // 스와이프: 현재 계좌 인덱스
   const navigate = useNavigate();
   // 사용자 정보
-  const name = localStorage.getItem('name');
-  const profile = localStorage.getItem('profile');
+  const name = localStorage.getItem("name");
+  const profile = localStorage.getItem("profile");
   const [isNavOpen, setIsNavOpen] = useState(false); // 햄버거 메뉴바
   const [isLoading, setIsLoading] = useState(true); // 로딩 상태
 
- const userIdx = localStorage.getItem('userIdx');
-	const userEmail = localStorage.getItem('email') as string;
-	const [notificationCount, setNotificationCount] = useState(0);
+  const userIdx = localStorage.getItem("userIdx");
+  const userEmail = localStorage.getItem("email") as string;
+  const [notificationCount, setNotificationCount] = useState(0);
 
-	const getNotifications = async (userIdx: string) => {
-		try {
-			const response = await axios.get(
-				`${process.env.REACT_APP_BESERVERURI}/api/notification/${userIdx}`
-			);
-			console.log(response.data.data);
-			return response.data.data;
-		} catch (error) {
-			console.error(error);
-			return null;
-		}
-	};
+  const getNotifications = async (userIdx: string) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_BESERVERURI}/api/notification/${userIdx}`
+      );
+      console.log(response.data.data);
+      return response.data.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
 
-	// useEffect(() => {
-	// 	const fetchNotifications = async () => {
-	// 		if (userIdx) {
-	// 			const notificationRes = await getNotifications(userIdx);
-	// 			setNotificationCount(notificationRes.length);
-	// 		}
-	// 	};
-	// 	fetchNotifications();
+  useEffect(() => {
+    const userIdx = localStorage.getItem("userIdx");
 
-		// SSE
-		// if (userIdx && userEmail) {
-		// 	const urlEndPoint = `http://${process.env.REACT_APP_BESERVERURI}/api/notification/subscribe/${userEmail}`;
-		// 	let eventSource = new EventSource(urlEndPoint);
+    if (!userIdx) {
+      console.error("User is not logged in or access token is missing");
+      return;
+    }
 
-		// 	console.log(urlEndPoint);
+    const url = `${process.env.REACT_APP_BESERVERURI}/api/user/accounts/list`;
 
-		// 	eventSource.onopen = () => {
-		// 		console.log('SSE connection opened');
-		// 	};
+    axios
+      .get(url, {
+        params: {
+          userIdx: userIdx,
+        },
+      })
+      .then((response) => {
+        if (response.data && response.data.data) {
+          setAccounts(response.data.data);
+        } else {
+          console.error("No data found in response", response);
+        }
+        setIsLoading(false); // 데이터 로드 완료
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false); // 데이터 로드 오류
+      });
+  }, [userIdx]);
 
-		// 	eventSource.onerror = (error) => {
-		// 		console.error('SSE error', error);
-		// 	};
+  // 페이지 이동 함수
+  const handleNavigate = (path: To) => {
+    navigate(path);
+  };
 
-		// 	eventSource.addEventListener('sse', (event) => {
-		// 		try {
-		// 			console.log(event.data);
-		// 			const result = JSON.parse(event.data);
-		// 			console.log('Received notification:', result);
-		// 			// setNotificationCount((prevCount) => prevCount + 1);
-		// 		} catch (error) {
-		// 			console.error('Error parsing JSON:', error);
-		// 		}
-		// 	});
-		// }
+  // 스와이프 이벤트 핸들러
+  const handlers = useSwipeable({
+    onSwipedLeft: () =>
+      setCurrentIndex((prev) => Math.min(prev + 1, accounts.length - 1)),
+    onSwipedRight: () => setCurrentIndex((prev) => Math.max(prev - 1, 0)),
+    trackMouse: true,
+  });
 
-		// if (!userIdx) {
-		// 	console.error('User is not logged in or access token is missing');
-		// 	return;
-		// }
-    useEffect(() => {
-      const userIdx = localStorage.getItem('userIdx');
-  
-      if (!userIdx) {
-        console.error('User is not logged in or access token is missing');
-        return;
-      }
-  
-		const url = `${process.env.REACT_APP_BESERVERURI}/api/user/accounts/list`;
-
-		axios
-			.get(url, {
-				params: {
-					userIdx: userIdx,
-				},
-			})
-			.then((response) => {
-				if (response.data && response.data.data) {
-					setAccounts(response.data.data);
-					// console.log('Accounts data:', response.data.data); // accounts 데이터 출력
-				} else {
-					console.error('No data found in response', response);
-				}
-				setIsLoading(false); // 데이터 로드 완료
-			})
-
-			.catch((error) => {
-				console.error(error);
-				setIsLoading(false); // 데이터 로드 오류
-			});
-	}, [userIdx]);
-
-	// 페이지 이동 함수
-	const handleNavigate = (path: To) => {
-		navigate(path);
-	};
-
-	// 스와이프 이벤트 핸들러
-	const handlers = useSwipeable({
-		onSwipedLeft: () =>
-			setCurrentIndex((prev) => Math.min(prev + 1, accounts.length - 1)),
-		onSwipedRight: () => setCurrentIndex((prev) => Math.max(prev - 1, 0)),
-		trackMouse: true,
-	});
-  // 0707 충돌에 의해 제거된 함수 추가
   // 계좌 클릭 이벤트 핸들러
   const handleAccountClick = (account: Account) => {
-    if (account.accountType === 'moaclub') {
+    if (account.accountType === "moaclub") {
       navigate(`/moaclub/main/${account.accountId}`);
     } else {
-      // 다른 계좌 타입에 대한 처리
+      // todo: 다른 계좌 타입에 대한 처리(셀럽, 입출금 추가 필요)
       navigate(`/account/${account.accountId}`);
     }
   };
-	// console.log(
-	// 	'localStorage에 저장된 userIdx:',
-	// 	localStorage.getItem('userIdx')
-	// );
-	// console.log('localStorage에 저장된 name:', localStorage.getItem('name'));
-	// console.log('localStorage에 저장된 email:', localStorage.getItem('email'));
-	// console.log(
-	// 	'localStorage에 저장된 profile:',
-	// 	localStorage.getItem('profile')
-	// );
+
+  // 보내기 버튼 클릭 이벤트 핸들러
+  const handleSendClick = (account: Account) => {
+    if (account.accountType === "moaclub") {
+      navigate(`/moaclub/deposit/${account.accountId}`);
+    } else {
+      // todo: 다른 계좌 타입에 대한 계좌이체 처리(셀럽, 입출금 추가 필요)
+      navigate(`/cash-out/${account.accountId}`);
+    }
+  };
 
   return (
     <>
-      <div className='mainPage'>
-        <div className='header'>
-          <div className='profile-pic'>
+      <div className="mainPage">
+        <div className="header">
+          <div className="profile-pic">
             {profile ? (
-              <img className='profile-pic' src={profile} alt='profile-pic' />
+              <img className="profile-pic" src={profile} alt="profile-pic" />
             ) : (
-              <img className='default-pic' src={hanaLogo} alt='default-pic' />
+              <img className="default-pic" src={hanaLogo} alt="default-pic" />
             )}
           </div>
-          <div className='user-name'>{name} 님</div>
-          <div className='menu-icon' onClick={() => setIsNavOpen(true)}>
-            {' '}
+          <div className="user-name">{name} 님</div>
+          <div className="menu-icon" onClick={() => setIsNavOpen(true)}>
+            {" "}
             {/* 햄버거 메뉴바 부분 */}
-            <img src={menuIcon} alt='menu-icon' />
+            <img src={menuIcon} alt="menu-icon" />
           </div>
         </div>
 
-        <div className='account-container' {...handlers}>
+        <div className="account-container" {...handlers}>
           <div
-            className='account-details'
+            className="account-details"
             style={{
               transform:
                 accounts.length > 0
                   ? `translateX(-${currentIndex * 100}%)`
-                  : 'none',
+                  : "none",
             }}
           >
             {isLoading ? (
@@ -191,39 +147,45 @@ const MainPage = () => {
                 {accounts.length > 0 ? (
                   accounts.map((account) => (
                     <div
-                    className='accountBox'
-                    key={account.accountId}
-                    onClick={() => handleAccountClick(account)}
-                  >
-                      <div className='imgContainer'>
-                        <img src={hanaLogo} alt='logo' />
+                      className="accountBox"
+                      key={account.accountId}
+                      onClick={() => handleAccountClick(account)}
+                    >
+                      <div className="imgContainer">
+                        <img src={hanaLogo} alt="logo" />
                       </div>
-                      <div className='accountDetail'>
-                        <p className='account-type'>
+                      <div className="accountDetail">
+                        <p className="account-type">
                           {account.name}
-                          {account.accountType === 'deposit' ? '의 통장' : ''}
+                          {account.accountType === "deposit" ? "의 통장" : ""}
                         </p>
-                        <p className='account-number'>{account.accountId}</p>
-                        <h3 className='account-balance'>
+                        <p className="account-number">{account.accountId}</p>
+                        <h3 className="account-balance">
                           {account.balance.toLocaleString()}원
                         </h3>
-                        <button className='send-button' onClick={() => {}}>
+                        <button
+                          className="send-button"
+                          onClick={(e) => {
+                            e.stopPropagation(); // 계좌 클릭 이벤트 버블링 방지
+                            handleSendClick(account);
+                          }}
+                        >
                           보내기
                         </button>
                       </div>
                     </div>
                   ))
                 ) : (
-                  <div className='accountBox'>
+                  <div className="accountBox">
                     <div>
-                      <img src={hanaLogo} alt='logo' />
+                      <img src={hanaLogo} alt="logo" />
                     </div>
-                    <div className='accountDetail'>
-                      <p className='account-type'>계좌가 없습니다.</p>
+                    <div className="accountDetail">
+                      <p className="account-type">계좌가 없습니다.</p>
                       <button
-                        id='basicBtn1'
-                        className='send-button'
-                        onClick={() => handleNavigate('/deposit')}
+                        id="basicBtn1"
+                        className="send-button"
+                        onClick={() => handleNavigate("/deposit")}
                       >
                         계좌 추가하기
                       </button>
@@ -233,10 +195,10 @@ const MainPage = () => {
               </>
             )}
           </div>
-          <div className='scrollbar'>
+          <div className="scrollbar">
             {accounts.length > 0 && (
               <div
-                className='scrollbar-indicator'
+                className="scrollbar-indicator"
                 style={{
                   width: `${100 / accounts.length}%`,
                   transform: `translateX(${currentIndex * 100}%)`,
@@ -246,37 +208,38 @@ const MainPage = () => {
           </div>
         </div>
 
-        <div className='promotions'>
-          <div className='promotion'>
-            <img src={celubIcon} alt='celubIcon' />
+        <div className="promotions">
+          <div className="promotion">
+            <img src={celubIcon} alt="celubIcon" style={{width:"3rem"}} />
 
-            <div className='promotionDetail'>
-              <p className='promotionSubTitle'>최애와 함께 저축 습관 들이기!</p>
-              <button onClick={() => handleNavigate('/celub/')}>
+            <div className="promotionDetail">
+              <p className="promotionSubTitle">최애와 함께 저축 습관 들이기!</p>
+              <button onClick={() => handleNavigate("/celub/")}>
                 셀럽로그 시작하기
               </button>
             </div>
+
           </div>
-          <div className='promotion'>
-            <img src={moaIcon} alt='moaIcon' />
-            <div className='promotionDetail'>
-              <p className='promotionSubTitle'>
+          <div className="promotion">
+            <img src={moaIcon} alt="moaIcon" style={{width:"3rem"}}/>
+            <div className="promotionDetail">
+              <p className="promotionSubTitle">
                 최애가 같다면 함께 쓰는 모임통장!
               </p>
-              <button onClick={() => handleNavigate('/moaclub/opening')}>
+              <button onClick={() => handleNavigate("/moaclub/opening")}>
                 모아클럽 시작하기
               </button>
             </div>
           </div>
 
           {/* // TODO: 실시간 환율 정보  */}
-          <div className='exchange-container'>
+          <div className="exchange-container">
             <Exchange />
           </div>
         </div>
       </div>
       <MenuBar />
-      <NavBar isOpen={isNavOpen} onClose={() => setIsNavOpen(false)} />{' '}
+      <NavBar isOpen={isNavOpen} onClose={() => setIsNavOpen(false)} />{" "}
       {/* 추가된 부분 */}
     </>
   );
