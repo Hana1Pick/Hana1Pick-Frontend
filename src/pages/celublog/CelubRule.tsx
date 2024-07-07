cf90188777765e5c899c765dde7949ce470e4f33
soyeonvv <sovvworld@gmail.com>
2024-07-08 02:31:05 +0900

    fix: 헤더 수정

--- src/pages/celublog/CelubRule.tsx
+++ src/pages/celublog/CelubRule.tsx
@@ -1,121 +1,137 @@
-import { useState, ChangeEvent, useEffect } from "react";
-import CelubHeader1 from "../../layouts/CelubHeader1";
-import { CelubRuleType } from "../../type/commonType";
-import axios from "axios";
-import { useLocation, useNavigate } from "react-router-dom";
-import "./CelublogStyle.scss";
+import { useState, ChangeEvent, useEffect } from 'react';
+import CelubHeader1 from '../../components/Header';
+import { CelubRuleType } from '../../type/commonType';
+import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './CelublogStyle.scss';
 import qs from 'qs';
-import CommonModal1 from "../../components/modal/CommonModal3";
+import CommonModal1 from '../../components/modal/CommonModal3';
 
 function CelubRule() {
-    const location = useLocation();
-    const {accountId, ruleList} = location.state;
-    console.log(accountId);
-    const navigate = useNavigate();
-    const [rules, setRules] = useState<CelubRuleType[]>(ruleList ||[]);
-    const [look, setLook] = useState(false);
-    useEffect(() => {
-        axios.post(`${process.env.REACT_APP_BESERVERURI}/api/celub/list/detail`,
-            qs.stringify({accountId:accountId}))
-            .then((res)=>{
-                console.log(res.data.data);
-                				setRules(
-					res.data.data.ruleInfo.map((rule:any) => ({
+	const location = useLocation();
+	const { accountId, ruleList } = location.state;
+	console.log(accountId);
+	const navigate = useNavigate();
+	const [rules, setRules] = useState<CelubRuleType[]>(ruleList || []);
+	const [look, setLook] = useState(false);
+	useEffect(() => {
+		axios
+			.post(
+				`${process.env.REACT_APP_BESERVERURI}/api/celub/list/detail`,
+				qs.stringify({ accountId: accountId })
+			)
+			.then((res) => {
+				console.log(res.data.data);
+				setRules(
+					res.data.data.ruleInfo.map((rule: any) => ({
 						ruleName: rule.ruleName,
-						ruleMoney: rule.ruleMoney
+						ruleMoney: rule.ruleMoney,
 					}))
-				)
-            }).catch((error)=>{
-                console.log("실패");
-            });
-    }, []); 
-    const addInput = () => {
-        if(rules.length>=10){
-            alert('규칙은 최대 10개 까지만 생성 가능합니다.');
-            return;
-        }
-        setRules([...rules, { ruleName: '', ruleMoney: 0 }]);
-    };
+				);
+			})
+			.catch((error) => {
+				console.log('실패');
+			});
+	}, []);
+	const addInput = () => {
+		if (rules.length >= 10) {
+			alert('규칙은 최대 10개 까지만 생성 가능합니다.');
+			return;
+		}
+		setRules([...rules, { ruleName: '', ruleMoney: 0 }]);
+	};
 
-    const handleInputChange = (
-        index: number,
-        event: ChangeEvent<HTMLInputElement>
-    ) => {
-        const { name, value } = event.target;
-        const newRules = [...rules];
-        if (name === 'ruleName') {
-            newRules[index].ruleName = value;
-        } else if (name === 'ruleMoney') {
-            newRules[index].ruleMoney = parseFloat(value);
-        }
-        setRules(newRules);
-        console.log(rules);
-    };
-    const addRules=()=>{
-        let data = {
-            accountId: accountId,
-            ruleList: rules.map(rule => ({
-                ruleName: rule.ruleName,
-                ruleMoney: rule.ruleMoney
-            }))
-        }
-        axios.post(`${process.env.REACT_APP_BESERVERURI}/api/celub/rule`,
-            data)
-            .then((res)=>{
-                console.log("확인하는중"+res.data.data);
-                setLook(true);
-                
-            }).catch((error)=>{
-                console.log("실패");
-            });
-    }
-    const completeChange = () =>{
-        navigate("/celub/detail",{state:accountId});
-    }
+	const handleInputChange = (
+		index: number,
+		event: ChangeEvent<HTMLInputElement>
+	) => {
+		const { name, value } = event.target;
+		const newRules = [...rules];
+		if (name === 'ruleName') {
+			newRules[index].ruleName = value;
+		} else if (name === 'ruleMoney') {
+			newRules[index].ruleMoney = parseFloat(value);
+		}
+		setRules(newRules);
+		console.log(rules);
+	};
+	const addRules = () => {
+		let data = {
+			accountId: accountId,
+			ruleList: rules.map((rule) => ({
+				ruleName: rule.ruleName,
+				ruleMoney: rule.ruleMoney,
+			})),
+		};
+		axios
+			.post(`${process.env.REACT_APP_BESERVERURI}/api/celub/rule`, data)
+			.then((res) => {
+				console.log('확인하는중' + res.data.data);
+				setLook(true);
+			})
+			.catch((error) => {
+				console.log('실패');
+			});
+	};
+	const completeChange = () => {
+		navigate('/celub/detail', { state: accountId });
+	};
 
-    return (
-        <>
-            <CelubHeader1 />
-            <div className="celub-rule-totalbox">
-                <div>
-                    <div className="celub-rulemake-box1">
-                        <h2>어떻게 모을지 <br />
-                            규칙을 설정해주세요.</h2>
-                    </div>
-                    <div className="celub-rulemake-box2">
-                        <p>최대 50만원씩 10개까지 만들 수 있어요. <br />
-                            규칙은 나중에도 수정할 수 있어요.</p>
-                    </div>
-                </div>
-                {rules.map((rule, index) => (
-                    <div key={index} className="celub-rulemake-box3">
-                        <input
-                            placeholder="규칙을 입력해주세요."
-                            className="celub-rulemake-input1"
-                            type="text"
-                            name="ruleName"
-                            value={rule.ruleName}
-                            onChange={(event) => handleInputChange(index, event)}
-                        />
-                        <input
-                            placeholder="금액을 입력해주세요."
-                            className="celub-rulemake-input2"
-                            type="number"
-                            name="ruleMoney"
-                            value={rule.ruleMoney}
-                            onChange={(event) => handleInputChange(index, event)}
-                        />
-                    </div>
-                ))}
+	return (
+		<>
+			<CelubHeader1 value='규칙 설정' />
+			<div className='celub-rule-totalbox'>
+				<div>
+					<div className='celub-rulemake-box1'>
+						<h2>
+							어떻게 모을지 <br />
+							규칙을 설정해주세요.
+						</h2>
+					</div>
+					<div className='celub-rulemake-box2'>
+						<p>
+							최대 50만원씩 10개까지 만들 수 있어요. <br />
+							규칙은 나중에도 수정할 수 있어요.
+						</p>
+					</div>
+				</div>
+				{rules.map((rule, index) => (
+					<div key={index} className='celub-rulemake-box3'>
+						<input
+							placeholder='규칙을 입력해주세요.'
+							className='celub-rulemake-input1'
+							type='text'
+							name='ruleName'
+							value={rule.ruleName}
+							onChange={(event) => handleInputChange(index, event)}
+						/>
+						<input
+							placeholder='금액을 입력해주세요.'
+							className='celub-rulemake-input2'
+							type='number'
+							name='ruleMoney'
+							value={rule.ruleMoney}
+							onChange={(event) => handleInputChange(index, event)}
+						/>
+					</div>
+				))}
 
-                <div className="celub-rulemake-box4">
-                    <button className="celub-addrule-btn" onClick={addInput}>추가</button>
-                    <button id="basicBtn1" onClick={addRules}>완료</button>
-                </div>
-            </div>
-            <CommonModal1 msg=" 규칙이 변경되었습니다." show={look} onConfirm={completeChange} />
-        </>
-    );
+				<div className='celub-rulemake-box4'>
+					<button className='celub-addrule-btn' onClick={addInput}>
+						추가
+					</button>
+					<button id='basicBtn1' onClick={addRules}>
+						완료
+					</button>
+				</div>
+			</div>
+			<CommonModal1
+				msg=' 규칙이 변경되었습니다.'
+				show={look}
+				onConfirm={completeChange}
+			/>
+		</>
+	);
 }
 
 export default CelubRule;
--- src/pages/celublog/CelubSearch.tsx
+++ src/pages/celublog/CelubSearch.tsx
@@ -1,128 +1,162 @@
-import React, { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import Header from "../../components/Header";
-import searchImg from "../../assets/images/celub/search.png";
-import axios from "axios";
-import { CelubListType, CelubWithdrawType } from "../../type/commonType";
-import "./CelublogStyle.scss";
-import CommonModal from "../../components/modal/CommonModal2";
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Header from '../../components/Header';
+import searchImg from '../../assets/images/celub/search.png';
+import axios from 'axios';
+import { CelubListType, CelubWithdrawType } from '../../type/commonType';
+import './CelublogStyle.scss';
+import CommonModal from '../../components/modal/CommonModal2';
 
 function CelubSearch() {
-  const userIdx = localStorage.getItem("userIdx");
-  const [look, setLook] = useState(false);
-  const location = useLocation();
-  const celubList = location.state;
-  const [selectJob, setSelectJob] = useState<string>("ACTOR");
-  const [searchResult, setSearchResult] = useState<CelubListType[]>([]);
-  const navigate = useNavigate();
-  const [keyword, setKeyword] = useState<string>("");
-  const [celubWithdraw, setCelubWithdraw] = useState<CelubWithdrawType>({
-    userIdx: userIdx ?? "",
-    accPw: 0,
-    name: "",
-    imgSrc: "",
-    outAccId: "",
-    celebrityIdx: 0,
-  });
-  const [celubIdx, setCelubIdx] = useState(0);
-  const [imgSrc, setImgSrc] = useState('');
-  const [selectedProfile, setSelectedProfile] = useState<number | null>(null); // 선택된 프로필 인덱스를 추적
+	const userIdx = localStorage.getItem('userIdx');
+	const [look, setLook] = useState(false);
+	const location = useLocation();
+	const celubList = location.state;
+	const [selectJob, setSelectJob] = useState<string>('ACTOR');
+	const [searchResult, setSearchResult] = useState<CelubListType[]>([]);
+	const navigate = useNavigate();
+	const [keyword, setKeyword] = useState<string>('');
+	const [celubWithdraw, setCelubWithdraw] = useState<CelubWithdrawType>({
+		userIdx: userIdx ?? '',
+		accPw: 0,
+		name: '',
+		imgSrc: '',
+		outAccId: '',
+		celebrityIdx: 0,
+	});
+	const [celubIdx, setCelubIdx] = useState(0);
+	const [imgSrc, setImgSrc] = useState('');
+	const [selectedProfile, setSelectedProfile] = useState<number | null>(null); // 선택된 프로필 인덱스를 추적
 
-  const jobChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectJob(e.target.value);
-  };
+	const jobChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setSelectJob(e.target.value);
+	};
 
-  const searchCelub = () => {
-    if (keyword == null || keyword === "") {
-      alert("검색어를 입력해주세요");
-      return;
-    }
-    axios
-      .get(`${process.env.REACT_APP_BESERVERURI}/api/celub/list/search`, {
-        params: {
-          userIdx: userIdx,
-          type: selectJob,
-          name: keyword,
-        },
-      })
-      .then((res) => {
-        console.log("검색리스트", res.data.data);
-        setSearchResult(res.data.data);
-      })
-      .catch((error) => {
-        alert("실패");
-      });
-  };
+	const searchCelub = () => {
+		if (keyword == null || keyword === '') {
+			alert('검색어를 입력해주세요');
+			return;
+		}
+		axios
+			.get(`${process.env.REACT_APP_BESERVERURI}/api/celub/list/search`, {
+				params: {
+					userIdx: userIdx,
+					type: selectJob,
+					name: keyword,
+				},
+			})
+			.then((res) => {
+				console.log('검색리스트', res.data.data);
+				setSearchResult(res.data.data);
+			})
+			.catch((error) => {
+				alert('실패');
+			});
+	};
 
-  const searchName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setKeyword(event.target.value);
-  };
+	const searchName = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setKeyword(event.target.value);
+	};
 
-  const selectCelub = (celubIdx: number, imgSrc: string, index: number) => {
-    setCelubIdx(celubIdx);
-    setImgSrc(imgSrc);
-    setSelectedProfile(index); // 선택된 프로필 인덱스를 설정
-    setLook(true);
-  };
+	const selectCelub = (celubIdx: number, imgSrc: string, index: number) => {
+		setCelubIdx(celubIdx);
+		setImgSrc(imgSrc);
+		setSelectedProfile(index); // 선택된 프로필 인덱스를 설정
+		setLook(true);
+	};
 
-  const isOk = () =>{
-    setCelubWithdraw((prevData) => ({
-      ...prevData,
-      celebrityIdx: celubIdx,
-      imgSrc: imgSrc,
-    }));
-    navigate("/celub/withdraw", {
-      state: { ...celubWithdraw, celebrityIdx: celubIdx, imgSrc: imgSrc },
-    });
-  }
-  const cancleBtn = () =>{
-    setLook(false);
-  }
+	const isOk = () => {
+		setCelubWithdraw((prevData) => ({
+			...prevData,
+			celebrityIdx: celubIdx,
+			imgSrc: imgSrc,
+		}));
+		navigate('/celub/withdraw', {
+			state: { ...celubWithdraw, celebrityIdx: celubIdx, imgSrc: imgSrc },
+		});
+	};
+	const cancleBtn = () => {
+		setLook(false);
+	};
 
-  return (
-    <>
-      <Header value="최애 선택" />
-      <CommonModal msg="최애를 선택하시겠습니까?" show={look} onCancle={cancleBtn} onConfirm={isOk} />
-      <div className="celub-search-box">
-        <select name="job" value={selectJob} onChange={jobChange}>
-          <option value="ACTOR">배우</option>
-          <option value="IDOL">아이돌</option>
-          <option value="ATHLETE">운동선수</option>
-        </select>
-        <input type="text" onChange={searchName} placeholder="원하는 셀럽을 입력하세요" />
-        <img className="celub-search-img" src={searchImg} onClick={searchCelub} alt="search" />
-      </div>
+	return (
+		<>
+			<Header value='최애 선택' />
+			<CommonModal
+				msg='최애를 선택하시겠습니까?'
+				show={look}
+				onCancle={cancleBtn}
+				onConfirm={isOk}
+			/>
+			<div className='celub-search-box'>
+				<select name='job' value={selectJob} onChange={jobChange}>
+					<option value='ACTOR'>배우</option>
+					<option value='IDOL'>아이돌</option>
+					<option value='ATHLETE'>운동선수</option>
+				</select>
+				<input
+					type='text'
+					onChange={searchName}
+					placeholder='원하는 셀럽을 입력하세요'
+				/>
+				<img
+					className='celub-search-img'
+					src={searchImg}
+					onClick={searchCelub}
+					alt='search'
+				/>
+			</div>
 
-      <div className="celub-profile-box">
-        {!searchResult && celubList.length === 0 ? (
-          <div className="celub-rule-box1">
-            선택할 수 있는 연예인이 없습니다. <br />
-            다시 검색해주세요.
-          </div>
-        ) : (
-          <div className="celub-profile-cover">
-            {searchResult.length === 0
-              ? celubList.map((celub: CelubListType, index: number) => (
-                  <div className="celub-profile-container" key={index} onClick={() => selectCelub(celub.idx, celub.thumbnail, index)}>
-                    <div className="celub-profile">
-                      <img className={selectedProfile === index ? 'selected' : ''} src={celub.thumbnail} alt={celub.name} />
-                      <p>{celub.name}</p>
-                    </div>
-                  </div>
-                ))
-              : searchResult.map((celub: CelubListType, index: number) => (
-                  <div className="celub-profile-container" key={index} onClick={() => selectCelub(celub.idx, celub.thumbnail, index)}>
-                    <div className="celub-profile">
-                      <img className={selectedProfile === index ? 'selected' : ''} src={celub.thumbnail} alt={celub.name} />
-                      <p>{celub.name}</p>
-                    </div>
-                  </div>
-                ))}
-          </div>
-        )}
-      </div>
-    </>
-  );
+			<div className='celub-profile-box'>
+				{!searchResult && celubList.length === 0 ? (
+					<div className='celub-rule-box1'>
+						선택할 수 있는 연예인이 없습니다. <br />
+						다시 검색해주세요.
+					</div>
+				) : (
+					<div className='celub-profile-cover'>
+						{searchResult.length === 0
+							? celubList.map((celub: CelubListType, index: number) => (
+									<div
+										className='celub-profile-container'
+										key={index}
+										onClick={() =>
+											selectCelub(celub.idx, celub.thumbnail, index)
+										}
+									>
+										<div className='celub-profile'>
+											<img
+												className={selectedProfile === index ? 'selected' : ''}
+												src={celub.thumbnail}
+												alt={celub.name}
+											/>
+											<p>{celub.name}</p>
+										</div>
+									</div>
+								))
+							: searchResult.map((celub: CelubListType, index: number) => (
+									<div
+										className='celub-profile-container'
+										key={index}
+										onClick={() =>
+											selectCelub(celub.idx, celub.thumbnail, index)
+										}
+									>
+										<div className='celub-profile'>
+											<img
+												className={selectedProfile === index ? 'selected' : ''}
+												src={celub.thumbnail}
+												alt={celub.name}
+											/>
+											<p>{celub.name}</p>
+										</div>
+									</div>
+								))}
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default CelubSearch;
