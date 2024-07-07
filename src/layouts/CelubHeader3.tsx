6b0f21ea098f580050c1f70b0c5a9ed27aa08906
juhye <rlawngp124@gmail.com>
2024-07-07 23:43:02 +0900

    refactor: modify css

--- src/layouts/CelubHeader3.tsx
+++ src/layouts/CelubHeader3.tsx
@@ -1,16 +1,18 @@
 import arrow from'../assets/images/celub/arrow.png';
-import setting from '../assets/images/celub/setting.png';
+import { useNavigate } from 'react-router-dom';
 
 function CelubHeader3({onClick}: {onClick: () => void }){
-
+    const navigate = useNavigate(); // useNavigate 훅 사용
+    const handleBackClick = () => {
+      navigate(-1); // 뒤로 가기
+    };
     return(
         <>
             <div className="celubHeader3">
                 <div>
-                    <img id="arrow_loc" src={arrow} />
+                    <img id="arrow_loc" src={arrow} onClick={handleBackClick}/>
                 </div>
                 <div>
-                    {/* <img id="setting" onClick={onClick} src={setting}/> */}
                     <h4 id="setting" onClick={onClick}>설정</h4>
                 </div>
             </div>
