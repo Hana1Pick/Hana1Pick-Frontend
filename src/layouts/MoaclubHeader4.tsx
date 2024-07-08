b14d0f7b7ef4999b7184dd963a648fc9989a4896
soyeonvv <sovvworld@gmail.com>
2024-07-08 12:26:14 +0900

    fix: 모아클럽 회비 규칙 충돌 해결

--- src/layouts/MoaclubHeader4.tsx
+++ src/layouts/MoaclubHeader4.tsx
@@ -3,19 +3,26 @@ import { MoaClubHeaderData } from '../type/commonType';
 import { useNavigate } from 'react-router-dom';
 
 function MoaClubHeader4({ value, disabled }: MoaClubHeaderData) {
-  const headerStyle = disabled ? { backgroundColor: 'rgba(0, 0, 0, 0.009)', borderColor: 'rgba(0, 0, 0, 0.009)' } : {};
-  const navigate = useNavigate(); // useNavigate 훅 사용
-  const handleBackClick = () => {
-    navigate(-1); // 뒤로 가기
-  };
-  return(
-    <>
-      <div className='moaclubHeader4' style={headerStyle}>
-        <img className="back" src={back} onClick={handleBackClick}/>
-        <div className="title" style={{fontWeight: "500", fontSize: "1.1rem"}}>{value}</div>
-      </div>
-    </>
-  );
+	const headerStyle = disabled
+		? {
+				backgroundColor: 'rgba(0, 0, 0, 0.009)',
+				borderColor: 'rgba(0, 0, 0, 0.009)',
+			}
+		: {};
+	const navigate = useNavigate(); // useNavigate 훅 사용
+	const handleBackClick = () => {
+		navigate(-1); // 뒤로 가기
+	};
+	return (
+		<>
+			<div className='moaclubHeader4' style={headerStyle}>
+				<img className='back' src={back} onClick={handleBackClick} />
+				<div className='title' style={{ fontWeight: '500', fontSize: '1rem' }}>
+					{value}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default MoaClubHeader4;
