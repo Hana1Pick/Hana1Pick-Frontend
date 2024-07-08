54d1a94ae20f48bf16405fccc188775f85f437f7
juhye <rlawngp124@gmail.com>
2024-07-08 16:04:35 +0900

    style: css, 에러 수정

--- src/pages/moaclub/MoaclubVoteTrsf.tsx
+++ src/pages/moaclub/MoaclubVoteTrsf.tsx
@@ -20,10 +20,10 @@ import { useTranslation } from 'react-i18next';
 
 function MoaclubVoteTrsf() {
   const { t, i18n } = useTranslation();
-	const [language, setLanguage] = useState(localStorage.getItem('language') || i18n.language);
+	const [language, setLanguage] = useState(localStorage.getItem('nation') || i18n.language);
   
 	useEffect(() => {
-    if(language=="KOR") i18n.changeLanguage('ko');
+    if(language=="Korea") i18n.changeLanguage('ko');
 	  else i18n.changeLanguage('ch');
 	}, [language, i18n]);
   const navigate = useNavigate();
